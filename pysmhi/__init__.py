1b07ca897186248807f90352f596b037e51ffa12
G Johansson <goran.johansson@shiftit.se>
2025-06-08 18:04:42 +0000

    Add fire forecasts to smhi

--- pysmhi/__init__.py
+++ pysmhi/__init__.py
@@ -2,14 +2,18 @@
 
 from __future__ import annotations
 
-from .exceptions import SMHIError, SmhiForecastException
+from .exceptions import SMHIError, SmhiFireForecastException, SmhiForecastException
 from .smhi import SmhiAPI
+from .smhi_fire_forecast import SMHIFireForecast, SMHIFirePointForecast
 from .smhi_forecast import SMHIForecast, SMHIPointForecast
 
 __all__ = [
     "SMHIError",
+    "SMHIFireForecast",
+    "SMHIFirePointForecast",
     "SMHIForecast",
     "SMHIPointForecast",
     "SmhiAPI",
+    "SmhiFireForecastException",
     "SmhiForecastException",
 ]
